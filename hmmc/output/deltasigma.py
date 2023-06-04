040c88d125cb50e0add62e2a2de7602c961c5b90
Charles-Henri Mousset <ch.mousset@gmail.com>
2023-06-04 22:40:06 +0200

    [enh] doc

--- hmmc/output/deltasigma.py
+++ hmmc/output/deltasigma.py
@@ -1,4 +1,14 @@
-# Delta Sigma DAC
+""" Delta Sigma DAC
+
+Delta Sigma modulators are simple Digital to Analog Converters. It transforms an integer in density
+of pulses.
+The modulated signal can be directly output on a digital pin, then with a simple RC low pass filter
+an analog voltage can be generated.
+
+Possible improvements:
+The modulator could easily support wider output, enabling the use of R2R resistor ladder to increase
+the analog resolution or bandwidth.
+"""
 from migen import Module, Signal, Cat
 
 
@@ -6,6 +16,15 @@ class DeltaSigma(Module):
     """Delta Sigma DAC
 
     Very simple modulator. Generates a bit stream at native clock speed.
+
+    parameter:
+    - resolution: resolution of the input data, in bits
+
+    inputs:
+    - input[resolution]: digital value to convert to pulse-density
+
+    outputs:
+    - output: pulse density modulated output
     """
     def __init__(self, resolution):
         self.input = Signal(resolution)
